c5f2388b8328d0b2b652728bd9453cb9bd09e0e4
Brett Sheleski <brett@sheleski.com>
2019-04-04 12:49:45 -0500

    initial commit

---
+++ MyFamilyList/MyFamilyList/AddFamilyMemberPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyFamilyList
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddFamilyMemberPage : ContentPage
+    {
+        public AddFamilyMemberPage()
+        {
+            InitializeComponent();
+        }
+
+        async void AddButton_Clicked(object sender, EventArgs e)
+        {
+            Person person = new Person();
+
+            person.FirstName = ???;
+            person.LastName = ???;
+            person.BirthDate = ???;
+
+            await Navigation.PopAsync();
+        }
+    }
+}
---
+++ MyFamilyList/MyFamilyList/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFamilyList
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
